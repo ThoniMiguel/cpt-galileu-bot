db17650a069f4ea8d5bb2e62652a7fe0d0c1e852
ThoniDesktop <wanok8000@outlook.com>
2021-03-10 02:51:41 -0300

    index changes

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: "ping",
-    description: "Simple Ping-Pong command!",
-    execute(msg, args){
-        //code goes here
-        msg.reply("Piu Piu Caralho!");
-    }
-}
+  name: "ping",
+  description: "Simple Ping-Pong command!",
+  execute(msg, args) {
+    //code goes here
+    msg.reply("Piu Piu Caralho!");
+  },
+};
