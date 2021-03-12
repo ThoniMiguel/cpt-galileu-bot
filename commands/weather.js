a4d00c7ab3f3ce1c738189c4cccab4959a3cd033
ThoniDesktop <wanok8000@outlook.com>
2021-03-12 14:49:33 -0300

    new weather-js

---
+++ commands/weather.js
@@ -0,0 +1,20 @@
+const { APIMessage } = require("discord.js");
+const fetch = require("node-fetch");
+const weather = require("weather-js");
+module.exports = {
+  name: "weather",
+  description: "Weather forecast",
+  execute(msg, args) {},
+};
+
+function weather(msg, args) {
+  let city = args[0];
+  weather.find({ search: city, degreeType: "C" }, (err, result) => {
+    if (err) console.log(err);
+    if (result.length === 0) {
+      msg.reply("Cidade nao encontrada! Ta dando uma de burro é?! PIU");
+    } else {
+      console.log(result);
+    }
+  });
+}
