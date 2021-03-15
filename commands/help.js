3de917b42243395ebac1faf92c1ec91a0ebe6714
ThoniMiguel <wanok8000@outlook.com>
2021-03-15 02:55:30 -0300

    add a better help

--- commands/help.js
+++ commands/help.js
@@ -1,29 +1,20 @@
-const path = require("path");
 const fs = require("fs");
 module.exports = {
-  name: "help",
-  description: "Show a list of available commands",
-  execute(client, msg, args) {
-    let commands = returnCommands();
-    let commandsCapitalized = new Array();
-    for(let i = 0; i < commands.length; i++){
-        commandsCapitalized[i] = commands[i].charAt(0).toUpperCase()+commands[i].slice(1);
+    name:"help",
+    description: "Show a list of available commands",
+    execute(client, msg, args, Discord){
+        const newEmbed = new Discord.MessageEmbed();
+        const commandFiles = fs
+        .readdirSync("./commands/")
+        .filter((file) => file.endsWith(".js"));
+
+        commandFiles.forEach(file => {
+            // console.log(`./${file}`);
+            let info = require(`./${file}`);
+            newEmbed.setColor('#7C00FF').setTitle('Comandos').setDescription("São esses os comandos por enquanto:")
+            .addFields({name: info.name, value: info.description});
+        });
+
+        msg.channel.send(newEmbed);
     }
-    msg.reply("Ta ai tua ajuda, seu merda:");
-    msg.channel.send("Comandos:\n"+commandsCapitalized.join(" - "));
-    // msg.channel.send("Facts, clear, ping, help, mimir");
-  },
-};
-
-function returnCommands() {
-  const commandFiles = fs
-    .readdirSync("./commands/")
-    .filter((file) => file.endsWith(".js"));
-
-    let temp = new Array();
-    for(let fileNames of commandFiles){
-        temp.push(fileNames.substr(0, fileNames.length-3));
-    }
-
-    return temp;
 }
